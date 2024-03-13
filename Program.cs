31ed9527e2c02344231d3dd5c5410e79536228dd
LKYfabian <jeremyfabian40@gmail.com>
2024-03-13 13:20:26 +0700

    Inisiasi Jurnal

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static;
+
+
+
+public class programs
+{ 
+enum state (apel, aprikot, alpukat, pisang, paprika, blackberry, ceri, kelapa, jagung);
+    public static void main(string[] args)
+    {
+    Console.WriteLine("list kelurahan");
+    foreach (buahfruit count in Enum GetValues )
+    
+
+
+
+    }
+
+public class kodebuah
+{
+enum state (A00, B00, C00, D00, E00, F00, H00, I00, J00);
+
+}
